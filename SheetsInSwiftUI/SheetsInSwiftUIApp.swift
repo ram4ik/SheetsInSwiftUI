597a9e6b0539dd5c1490765433c3c5496184d5c3
Ramill <ram4ik@hotmail.com>
2024-01-06 15:25:49 +0200

    Initial Commit

---
+++ SheetsInSwiftUI/SheetsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetsInSwiftUIApp.swift
+//  SheetsInSwiftUI
+//
+//  Created by admin on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SheetsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
