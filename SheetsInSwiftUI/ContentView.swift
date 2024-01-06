caafe66c28c55fb5c58e7e4215aaffa7abe8d413
Ramill <ram4ik@hotmail.com>
2024-01-06 15:38:20 +0200

    init

--- SheetsInSwiftUI/ContentView.swift
+++ SheetsInSwiftUI/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSheet = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                showSheet.toggle()
+            } label: {
+                Text("Click Me")
+            }
         }
-        .padding()
+//        .fullScreenCover(isPresented: $showSheet, content: {
+//            Button(action: {
+//                showSheet.toggle()
+//            }, label: {
+//                Text("Dismiss")
+//            })
+//        })
+        .sheet(isPresented: $showSheet, content: {
+            Text("Showed!")
+                .presentationDetents([.height(200), .medium, .large])
+//                .interactiveDismissDisabled()
+//                .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+                
+        })
     }
 }
 
